5f23169b8f0442642b97c015c746a332c4334a92
Valanarasu09 <37505597+Valanarasu09@users.noreply.github.com>
2020-03-13 14:07:11 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,43 @@
+public class Account{
+	
+	private int accountId;
+
+	private String accountType;
+
+	private int balance;
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public boolean withdraw(int amount ) {
+		if(balance>amount)
+		{
+			balance-=amount;
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+}
